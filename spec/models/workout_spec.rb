64640a56aa2aeceb7ec5fb3f6e98d8aa690a7c41
Alexa Zeazas <student@epicodus.com>
2015-10-29 09:57:29 -0700

    Test specs for Workout model, pass.

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-# RSpec.describe Workout, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Workout do
+  it { should belong_to :exercise }
+  it { should belong_to :user }
+end
