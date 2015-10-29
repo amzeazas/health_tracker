be306d1fea1288e190bdc3590dc133c2aa08f718
Alexa Zeazas <student@epicodus.com>
2015-10-29 09:41:39 -0700

    Test associations for User model, pass.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ describe User do
   it { should validate_presence_of :email }
   it { should validate_presence_of :username }
   it { should validate_uniqueness_of :username }
+  it { should have_many :meals }
+  it { should have_many :workouts }
+  it { should have_many(:foods).through(:meals) }
+  it { should have_many(:exercises).through(:workouts) }
 end
