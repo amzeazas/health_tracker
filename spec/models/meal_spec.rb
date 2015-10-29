8bf925ee5561643f5c879eee3d1a297691d49e73
Alexa Zeazas <student@epicodus.com>
2015-10-29 09:56:57 -0700

    Test specs for Meal model, pass.

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-# RSpec.describe Meal, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Meal do
+  it { should belong_to :food }
+  it { should belong_to :user }
+end
